60c44dcc3a5ff593e6e96fc733041726bd0f5ec1
LachlanHansfordAIE <s200014@students.aie.edu.au>
2022-07-05 10:04:47 +0930

    Always are ready for trying to put everything in for testing

--- WHODUNNIT/Assets/Scripts/Door/DoorOpenAndClosed.cs
+++ WHODUNNIT/Assets/Scripts/Door/DoorOpenAndClosed.cs
@@ -6,24 +6,20 @@ public class DoorOpenAndClosed : MonoBehaviour
 {
     public Animator door;
 
-    public bool opening;
+
 
     public void OnTriggerEnter(Collider other)
     {
         door.SetTrigger("open");
     }
 
-    public void OnTriggerStay(Collider other)
-    {
-        opening = true;
-    }
 
 
     public void OnTriggerExit(Collider other)
     {
         StartCoroutine(DoorClosed());
 
-        opening = false;
+
     }
 
 
@@ -32,12 +28,8 @@ public class DoorOpenAndClosed : MonoBehaviour
         yield return new WaitForSeconds(15f);
 
 
-        if(opening == false)
-        {
-            door.SetTrigger("closed");
+        door.SetTrigger("closed");
 
-
-        }
         
 
     }
